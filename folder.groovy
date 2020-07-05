bfb99217a38c09677b8322c3ca549d7d9db00c3f
rayinianji <anjaneyulu195@gmail.com>
2020-07-05 15:00:20 +0530

    added folder.groovy

--- folder.groovy
+++ folder.groovy
@@ -1,3 +1,15 @@
 folder('project-a/testing') {
     description('Folder containing all QA jobs for project A')
 }
+job('Webapp_Graddle') {
+    scm {
+        git {
+		
+            remote {
+                url('https://github.com/rayinianji/NewWebproject.git')
+				
+                }
+			branch('master')	
+        }
+    }
+}
