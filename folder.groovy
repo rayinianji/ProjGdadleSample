f3dea759e9460b38512088102c574710cce74dcc
rayinianji <anjaneyulu195@gmail.com>
2020-07-05 19:24:07 +0530

    updated

--- folder.groovy
+++ folder.groovy
@@ -1,9 +1,3 @@
 folder('ABC/Dev') {
     description('Folder containing all DEV jobs for project ABC')
 }
-folder('ABC/QA') {
-    description('Folder containing all QA jobs for project ABC')
-}
-folder('ABC/Prod') {
-    description('Folder containing all PROD jobs for project ABC')
-}
